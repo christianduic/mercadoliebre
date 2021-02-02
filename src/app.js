ed62905bcd55bad2267f5cf9db34e0ce45cfb8c5
christianduic <christianduic@hotmail.com>
2021-02-02 13:55:39 -0300

    Add configuration for deployment in Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const staticFolder = path.resolve(__dirname, "./public");
 
 app.use(express.static(staticFolder));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
